0a4e0d02ee2ad2404dc2d8fe1dc695e335985671
AlonsoNajera <108036725+AlonsoNajera@users.noreply.github.com>
2022-09-03 18:35:23 -0500

    d

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
 console.log("Entro al main.js");
+const textobusqueda=document.getElementById("textobusqueda");
+
+
+//FUNCIONES
+
+function buscar() {
+    console.log("entro a la funcion buscar");
+    console.log(textobusqueda.value);
+}
+
